abdda4e4cd1bb0d5b5659dc641d3a8cd6f39b46e
Juan Felipe Muñoz <juanfelipemh@gmail.com>
2023-06-14 20:39:12 -0500

    Ingresos ajuste

--- src/components/ingresos/Ingresos.jsx
+++ src/components/ingresos/Ingresos.jsx
@@ -9,7 +9,7 @@ import { formatearMoneda } from '../../helpers/formatoMoneda'
 import GraficoIngresos from '../grafico/GraficoIngresos'
 import Paginacion from '../../helpers/Paginacion'
 import { BACKEND_URL } from '../../config/url'
-import { Spinner } from '../../helpers/LoadingData'
+import { Spinner } from '../../helpers/Spinner'
 import { LoadingData } from '../../helpers/LoadingData'
 
 const Ingresos = () => {
